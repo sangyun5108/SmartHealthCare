cc5b07a8f7abfe3374e2eed4698cecd365387bb4
박상윤 <tkfkd4447@naver.com>
2021-11-21 22:59:13 +0900

    login페이지 구현

--- app/src/main/java/com/sypark/smarthealthcare/auth/AuthActivity.kt
+++ app/src/main/java/com/sypark/smarthealthcare/auth/AuthActivity.kt
@@ -3,15 +3,20 @@ package com.sypark.smarthealthcare.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.ImageView
-import androidx.fragment.app.FragmentContainerView
-import androidx.viewpager2.widget.ViewPager2
+import android.widget.TextView
 import com.sypark.smarthealthcare.R
+import org.w3c.dom.Text
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        val loginBtn = findViewById<TextView>(R.id.loginButton)
+
+        loginBtn.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/sypark/smarthealthcare/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.sypark.smarthealthcare.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sypark.smarthealthcare.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
