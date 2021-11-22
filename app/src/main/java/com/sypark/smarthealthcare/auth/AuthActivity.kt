b124d2959929627f52ac57e63b9bc044404ad669
박상윤 <tkfkd4447@naver.com>
2021-11-22 16:30:44 +0900

    회원가입 페이지 구현

--- app/src/main/java/com/sypark/smarthealthcare/auth/AuthActivity.kt
+++ app/src/main/java/com/sypark/smarthealthcare/auth/AuthActivity.kt
@@ -4,8 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.sypark.smarthealthcare.JoinActivity
 import com.sypark.smarthealthcare.R
-import org.w3c.dom.Text
+
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,9 +14,15 @@ class AuthActivity : AppCompatActivity() {
         setContentView(R.layout.activity_auth)
 
         val loginBtn = findViewById<TextView>(R.id.loginButton)
+        val JoinBtn = findViewById<TextView>(R.id.JoinButton)
 
         loginBtn.setOnClickListener {
             val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        JoinBtn.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
             startActivity(intent)
         }
     }
