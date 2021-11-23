ccf824e13f3846912266174ff22d67599c9376cd
박상윤 <tkfkd4447@naver.com>
2021-11-23 12:22:57 +0900

    회원가입 오류 로직 구현

--- app/src/main/java/com/sypark/smarthealthcare/auth/JoinActivity.kt
+++ app/src/main/java/com/sypark/smarthealthcare/auth/JoinActivity.kt
@@ -2,11 +2,79 @@ package com.sypark.smarthealthcare.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.sypark.smarthealthcare.R
 
 class JoinActivity : AppCompatActivity() {
+
+    private var permission:Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+
+        val email = findViewById<EditText>(R.id.joinEmail)
+        val name = findViewById<EditText>(R.id.joinName)
+        val password = findViewById<EditText>(R.id.joinPassword)
+        val passwordCheck = findViewById<EditText>(R.id.joinPasswordCheck)
+        val joinButton = findViewById<Button>(R.id.joinButton)
+
+        joinButton.setOnClickListener {
+            checkJoin(email,name,password,passwordCheck)
+        }
+
+    }
+
+
+    // 회원가입 오류 로직
+    private fun checkJoin(email:EditText, name:EditText, password:EditText, passwordCheck:EditText){
+
+        val emailTxt = email.text.toString()
+        val nameTxt = name.text.toString()
+        val passwordTxt = password.text.toString()
+        val passwordCheckTxt = passwordCheck.text.toString()
+
+        if(emailTxt.isEmpty()){
+            Toast.makeText(this,"이메일을 입력하세요",Toast.LENGTH_SHORT).show()
+            permission = false
+        }else{
+            if(!android.util.Patterns.EMAIL_ADDRESS.matcher(emailTxt).matches()){
+                Toast.makeText(this,"올바른 이메일 형식을 입력하세요",Toast.LENGTH_SHORT).show()
+                permission = false
+            }
+        }
+
+        if(nameTxt.isEmpty()){
+            Toast.makeText(this,"이름을 입력하세요",Toast.LENGTH_SHORT).show()
+            permission = false
+        }
+
+        if(passwordTxt.isEmpty()){
+            Toast.makeText(this,"비밀번호를 입력하세요",Toast.LENGTH_SHORT).show()
+            permission = false
+        }else{
+            if(password.length()<6){
+                Toast.makeText(this,"비밀번호를 6자리 이상 입력하세요",Toast.LENGTH_SHORT).show()
+                permission = false
+            }
+        }
+
+        if(passwordCheckTxt.isEmpty()){
+            Toast.makeText(this," 비밀번호 확인란을 입력하세요",Toast.LENGTH_SHORT).show()
+            permission = false
+        }else{
+            if(passwordTxt!=passwordCheckTxt){
+                Toast.makeText(this,"비밀번호가 다릅니다",Toast.LENGTH_SHORT).show()
+                permission = false
+            }
+        }
+
+        if(permission){
+            Log.d("permission",permission.toString())
+            Toast.makeText(this,"회원가입이 완료되었습니다.",Toast.LENGTH_SHORT).show()
+        }
     }
 }
