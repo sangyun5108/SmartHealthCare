946cb04398fe09664c0fd2265c52033a18c097f4
박상윤 <tkfkd4447@naver.com>
2021-11-23 12:04:45 +0900

    JoinActivity 파일경로 이동

--- app/src/main/java/com/sypark/smarthealthcare/auth/JoinActivity.kt
+++ app/src/main/java/com/sypark/smarthealthcare/auth/JoinActivity.kt
@@ -1,7 +1,8 @@
-package com.sypark.smarthealthcare
+package com.sypark.smarthealthcare.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sypark.smarthealthcare.R
 
 class JoinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
