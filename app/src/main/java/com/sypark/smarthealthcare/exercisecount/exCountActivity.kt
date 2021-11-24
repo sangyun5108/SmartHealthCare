9e49f3a7e8db39572f36b989eb0432f236d2cf26
박상윤 <tkfkd4447@naver.com>
2021-11-24 21:35:50 +0900

    운동 횟수 카운팅 페이지 구현

---
+++ app/src/main/java/com/sypark/smarthealthcare/exercisecount/exCountActivity.kt
@@ -0,0 +1,45 @@
+package com.sypark.smarthealthcare.exercisecount
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import com.sypark.smarthealthcare.R
+
+class exCountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ex_count)
+
+        val progressbar = findViewById<ProgressBar>(R.id.progressbar)
+        val button = findViewById<Button>(R.id.countButton)
+        val percent = findViewById<TextView>(R.id.percent)
+        val countNumber = findViewById<TextView>(R.id.countNumber)
+
+        var pStatus = progressbar.progress
+        var count = 0
+
+        button.setOnClickListener {
+            kotlin.concurrent.timer(period = 7, initialDelay = 0) {
+                runOnUiThread {
+                    percent.text="${pStatus+1}%"
+                }
+                progressbar.setProgress(++pStatus)
+                if(progressbar.progress==100){
+                    cancel()
+                    runOnUiThread {
+                        countNumber.text="Count:${++count}"
+                        percent.text="100%"
+                    }
+                    kotlin.concurrent.timer(period = 7, initialDelay = 0) {
+                        progressbar.setProgress(pStatus--)
+                        if(progressbar.progress==0){
+                            cancel()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
